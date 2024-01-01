0c779a97c214a11bf5cc7c13fd6af2a84c2b6fcc
JuanNahual <juan.nahueljg@gmail.com>
2024-01-01 18:04:02 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Error404 from 'containers/errors/Error404';
 import Home from 'containers/pages/Home';
 import store from './store';
@@ -6,30 +6,20 @@ import { Provider } from 'react-redux';
 import Contacto from 'containers/pages/Contacto';
 import Empresas from 'containers/pages/Empresas';
 
-
-
 function App() {
   return (
     <Provider store={store}>
-    <Router basename="/Grupos-sp">
-      <Router>
+      <Router basename="/Grupos-sp">
         <Routes>
-            
-
-            {/* Home Display */}
-            <Route path="/Home" element={<Home /> } />
-            <Route path="/Contacto" element={<Contacto /> } />
-            <Route path="/Empresas" element={<Empresas /> } />
-            {/* Error Display */}
-            <Route path="*" element={<Error404 /> } />
-
-
-
+          {/* Home Display */}
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Contacto" element={<Contacto />} />
+          <Route path="/Empresas" element={<Empresas />} />
+          {/* Error Display */}
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </Router>
-      </Router>
     </Provider>
-    
   );
 }
 
